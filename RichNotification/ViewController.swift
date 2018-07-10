9db6c1c33bb1fecc4c6e8b33ed35ac2dbe683346
saish98 <saish98@gmail.com>
2018-07-10 15:19:08 +0530

    Initial Commit

---
+++ RichNotification/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  RichNotification
+//
+//  Created by Heady on 10/07/18.
+//  Copyright © 2018 Heady. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
